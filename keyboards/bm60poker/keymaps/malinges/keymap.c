ef9ad088fb43e0cfd2d7b23abd62eb9e695ddd46
Simon Malinge <simon.malinge@gmail.com>
2021-02-17 23:57:16 -0500

    move _fn back to MO from TT / TG

--- keyboards/bm60poker/keymaps/malinges/keymap.c
+++ keyboards/bm60poker/keymaps/malinges/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPFN,                            KC_RALT, KC_RCTL, TT(_fn), TT(_fn)
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPFN,                            KC_RALT, KC_RCTL, MO(_fn), MO(_fn)
     ),
     // Gamer standard qwerty layout but with normal space for jumping etc
     [_gamer] = LAYOUT_60_ansi(
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, RGB_TOG, XXXXXXX, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_MOD, XXXXXXX, XXXXXXX, XXXXXXX, RESET,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_SPI, RGB_SPD, XXXXXXX, XXXXXXX,          XXXXXXX,
         KC_LSFT,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          KC_RSFT,
-        qwerty,  XXXXXXX, gamer,                              XXXXXXX,                            XXXXXXX, XXXXXXX, TG(_fn), TG(_fn)
+        qwerty,  XXXXXXX, gamer,                              XXXXXXX,                            XXXXXXX, XXXXXXX, _______, _______
     )
 };
 
