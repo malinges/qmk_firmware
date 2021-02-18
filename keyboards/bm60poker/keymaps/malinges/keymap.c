8504a7eaf23b63142c5bcb977b2849b4d8481698
Simon Malinge <simon.malinge@gmail.com>
2021-02-17 22:38:52 -0500

    replace CTL_T(KC_ESC) with KC_ESC

--- keyboards/bm60poker/keymaps/malinges/keymap.c
+++ keyboards/bm60poker/keymaps/malinges/keymap.c
@@ -29,14 +29,13 @@ gamer = SAFE_RANGE,
 qwerty,
 };
 
-#define CTL_ESC CTL_T(KC_ESC) // tap for Escape, hold for Control
 #define KC_SPFN LT(_spcfn, KC_SPC) // press for space, hold for function layer (aka spacefn)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_qwerty] = LAYOUT_60_ansi(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-        CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPFN,                            KC_RALT, KC_RCTL, MO(_fn), MO(_rgb)
     ),
