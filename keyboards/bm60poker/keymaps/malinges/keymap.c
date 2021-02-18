9f4703401733c73ca7d2aaddb8e379cb02523631
Simon Malinge <simon.malinge@gmail.com>
2021-02-17 22:28:41 -0500

    add delete rgb indicator

--- keyboards/bm60poker/keymaps/malinges/keymap.c
+++ keyboards/bm60poker/keymaps/malinges/keymap.c
@@ -107,8 +107,9 @@ void rgb_matrix_indicators_kb(void)
     if (!g_suspend_state && rgb_matrix_config.enable) {
         switch (get_highest_layer(layer_state)) {
             case _spcfn:
-                // Escape
+                // Escape and Delete
                 rgb_matrix_set_color(0, 0xFF, 0x00, 0x00);
+                rgb_matrix_set_color(13, 0xFF, 0x00, 0x00);
                 // Caps Lock
                 rgb_matrix_set_color(28, 0x66, 0x66, 0x66);
                 // Page Up and Page Down
