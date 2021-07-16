178cd75bf745b610f530add4ade5000d99d6757e
Simon Malinge <simon.malinge@gmail.com>
2021-07-16 01:03:57 -0400

    reviung41/malinges: shuffle some frequently-used keys around

--- keyboards/reviung41/keymaps/malinges/keymap.c
+++ keyboards/reviung41/keymaps/malinges/keymap.c
@@ -32,8 +32,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_reviung41(
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
     ALT_ESC,  KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  RSFT_T(KC_ENT),
-                                            KC_RGUI,   LOWER,    KC_SPC,   RAISE,    KC_RCTL
+    KC_LCTL,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RCTL,
+                                            KC_RGUI,   LOWER,    KC_SPC,   RAISE,    RSFT_T(KC_ENT)
   ),
 
   [_LOWER] = LAYOUT_reviung41(
