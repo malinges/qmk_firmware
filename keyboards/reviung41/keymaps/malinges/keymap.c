22523a27e59a616f3f22f44489db4fe6d8d1a3c5
Simon Malinge <simon.malinge@gmail.com>
2021-07-19 21:18:47 -0400

    reviung41/malinges: shave 2 bytes in oled_task_user()

--- keyboards/reviung41/keymaps/malinges/keymap.c
+++ keyboards/reviung41/keymaps/malinges/keymap.c
@@ -226,7 +226,7 @@ void oled_task_user(void) {
   led_t led_state = host_keyboard_led_state();
   oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
   oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
-  oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+  oled_write_P(led_state.scroll_lock ? PSTR("SCR") : PSTR("   "), false);
 }
 
 void keyboard_post_init_user(void) {
