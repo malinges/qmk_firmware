2769b55ea1db067c6d4d2807d4e87f08df657386
Simon Malinge <simon.malinge@gmail.com>
2021-07-21 18:00:04 -0400

    reviung41/malinges: display QMK logo on oled at startup

--- keyboards/reviung41/keymaps/malinges/keymap.c
+++ keyboards/reviung41/keymaps/malinges/keymap.c
@@ -101,6 +101,18 @@ typedef union {
 
 static user_config_t user_config;
 
+#  ifndef OLED_STARTUP_LOGO_DURATION
+#    define OLED_STARTUP_LOGO_DURATION 2000
+#  endif
+
+static uint16_t startup_logo_start;
+static const char PROGMEM qmk_logo[] = {
+    0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
+    0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
+    0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
+};
+static bool startup_logo_show = false;
+
 static bool is_oled_enabled(void) {
   return user_config.oled_enable;
 }
@@ -120,6 +132,10 @@ static void oled_toggle(void) {
 void keyboard_post_init_user(void) {
   user_config.raw = eeconfig_read_user();
   oled_enable(user_config.oled_enable);
+  if (user_config.oled_enable) {
+    startup_logo_start = timer_read();
+    startup_logo_show = true;
+  }
 }
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
@@ -158,6 +174,15 @@ static bool oled_write_rgb_mode_name(uint8_t mode_base, const char *data, bool m
 void oled_task_user(void) {
   if (!is_oled_enabled()) {
     return;
+  }
+
+  if (startup_logo_show) {
+    if (timer_elapsed(startup_logo_start) < OLED_STARTUP_LOGO_DURATION) {
+      oled_write_P(qmk_logo, false);
+      return;
+    } else {
+      startup_logo_show = false;
+    }
   }
 
 #  ifdef RGBLIGHT_ENABLE
